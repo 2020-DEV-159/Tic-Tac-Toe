46a7913f7186fdfd4fe4be9f0d7a8db64fc06419
2020-DEV-159 <2020-DEV-159@2020-DEV-159>
2020-09-29 13:23:19 +0530

    Added common instance to run both the test classes

--- src/test/java/com/bnpp/kata/game/TicTacToeTest.java
+++ src/test/java/com/bnpp/kata/game/TicTacToeTest.java
@@ -5,19 +5,16 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 public class TicTacToeTest {
+	TicTacToe ticTacToe = new TicTacToe();
 
 	@Test
 	public void givenFirstTurnPlayerItShouldBeX() {
-
-		TicTacToe ticTacToe = new TicTacToe();
 
 		assertThat(ticTacToe.firstPlayer(), CoreMatchers.is('X'));
 	}
 
 	@Test
 	public void givenSecondTurnPlayerItShouldBePlayerY() {
-
-		TicTacToe ticTacToe = new TicTacToe();
 
 		ticTacToe.play(2, 2); // X
 
