0d4bc33819c608c3de084ba0783d7d3b6317346f
2020-DEV-159 <2020-DEV-159@2020-DEV-159>
2020-09-29 13:18:04 +0530

    Verified : Player should be able to place Second player in any position of board

--- src/main/java/com/bnpp/kata/game/TicTacToe.java
+++ src/main/java/com/bnpp/kata/game/TicTacToe.java
@@ -1,8 +1,17 @@
 package com.bnpp.kata.game;
 
 public class TicTacToe {
-	
+	private char lastPlayer;
+
 	public char firstPlayer() {
 		return 'X' ;
+	}
+
+	public char SecondPlayer() {
+		return 'O';
+	}
+
+	public void play(int row, int column) {
+		lastPlayer = SecondPlayer();
 	}
 }
--- src/test/java/com/bnpp/kata/game/TicTacToeTest.java
+++ src/test/java/com/bnpp/kata/game/TicTacToeTest.java
@@ -5,12 +5,22 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 public class TicTacToeTest {
-	
+
 	@Test
 	public void givenFirstTurnPlayerItShouldBeX() {
 
 		TicTacToe ticTacToe = new TicTacToe();
 
 		assertThat(ticTacToe.firstPlayer(), CoreMatchers.is('X'));
+	}
+
+	@Test
+	public void givenSecondTurnPlayerItShouldBePlayerY() {
+
+		TicTacToe ticTacToe = new TicTacToe();
+
+		ticTacToe.play(2, 2); // X
+
+		assertThat(ticTacToe.SecondPlayer() ,CoreMatchers.is('O'));
 	}
 }
