cabbd8c11792498e752f60acae550c08599325d8
2020-DEV-159 <2020-DEV-159@2020-DEV-159>
2020-09-30 17:21:19 +0530

    Verified: Game should declare the result as possiblemoves when all the positions in board

--- src/main/java/com/bnpp/kata/TicTacToeRunner/Position.java
+++ src/main/java/com/bnpp/kata/TicTacToeRunner/Position.java
@@ -1,5 +1,8 @@
 package com.bnpp.kata.TicTacToeRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Position {
 	public static final int DIM = 3;
 	public static final int SIZE = DIM*DIM;
@@ -34,5 +37,14 @@ public class Position {
 		board[idx] = ' ';
 		turn = turn == 'X' ? 'O' : 'X';
 		return this;
+	}
+
+	public List<Integer> possibleMoves() {
+		List<Integer> list = new ArrayList<>();
+		for(int i=0; i < board.length ; i++) {
+			if(board[i] == ' ' )
+				list.add(i);	
+		}
+		return list;
 	}
 }
--- src/test/java/com/bnpp/kata/game/PositionTest.java
+++ src/test/java/com/bnpp/kata/game/PositionTest.java
@@ -1,11 +1,13 @@
 package com.bnpp.kata.game;
 
 import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 import com.bnpp.kata.TicTacToeRunner.Position;
 
 public class PositionTest {
-
+	
 	@Test
 	public void givenFirstPlayerPosition() throws Exception {
 
@@ -26,11 +28,24 @@ public class PositionTest {
 
 	@Test
 	public void givenPlayerUnmovePosition() throws Exception {
-		
+
 		Position position = new Position().move(1).unmove(1);
-		
+
 		assertEquals('X',position.turn);
 		assertEquals("         ", position.toString());
+	}
+
+	@Test
+	public void testPossibleMoves() throws Exception {
+
+		List<Integer> list = new ArrayList<>();	
+		for(int i=0; i < Position.SIZE ; i++) {
+			list.add(i);	
+		}
+		list.remove(new Integer(1));
+		list.remove(new Integer(2));	
+
+		assertEquals(list, new Position().move(1).move(2).possibleMoves());
 	}
 }
 
